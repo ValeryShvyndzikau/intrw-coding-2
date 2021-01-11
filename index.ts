afce234005093659bc0260d5b47bd623026812f3
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2021-01-11 23:46:18 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -18,4 +18,28 @@ function anagram(str1, str2) {
   return str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join('');
 }
 
-console.log(anagram("dell", "ledl"));
+// console.log(anagram("dell", "ledl"));
+
+// Fibonacci
+// ================================
+// prettier-ignore
+function fibonacci(num) {
+  let result = [0,1];
+
+  for (let i = 2; i <= num; i++) {
+    const left = result[i - 1];
+    const right = result[i - 2];
+
+    result.push(left + right);
+  }
+
+  return result[num];
+}
+
+// console.log(fibonacci(6));
+
+// function fibonacci2(num) {
+//   if (num <= 1) return 1;
+
+//   return fibonacci(num - 1) + fibonacci(num - 2);
+// }
