fb99ddb1b284d399a1f4bcfda22414987c0c015a
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2021-01-12 00:23:51 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -36,7 +36,8 @@ function fibonacci(num) {
   return result[num];
 }
 
-// console.log(fibonacci(6));
+// 0,1,1,2,3,5,8,13,21,34
+console.log(fibonacci(7));
 
 // function fibonacci2(num) {
 //   if (num <= 1) return 1;
